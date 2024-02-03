174394344e076903cd0c3ad61fb62c7a5d6359c5
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-03 17:08:50 +0530

    Write a program to calculate the area and perimeter of a rectangle

---
+++ 02.py
@@ -0,0 +1,8 @@
+l = float(input("Enter length of a rectangle: "))
+b = float(input("Enter breadth of a rectangle: "))
+
+a = l*b
+p = 2*(l+b)
+
+print("Area of the rectangle is:", a)
+print("Perimeter of the rectangle is:", p)
