a5f788c1dc6341699c97ee9a13d7cba10cbbc6fc
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-03 20:01:16 +0530

    Printing star pattern

--- 11.py
+++ 11.py
@@ -1,3 +1,4 @@
+# Pattern of numbers
 rows = int(input("Enter number of rows: "))
 num = 1
 
@@ -5,4 +6,15 @@ for i in range(1, rows+1):
     for j in range(i):
         print(num, end=" ")
         num += 1
+    print("\n")
+
+# Pattern of stars
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 0, -1):
+    for j in range(0, rows-i):
+        print(" ", end=" ")
+    
+    for j in range(0, 2*i-1):
+        print("* ", end=" ")
     print("\n")
