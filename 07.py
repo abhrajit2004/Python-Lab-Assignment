d1df648d4ffc048f9f39d7f2eb991c39f861d6ba
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-03 18:02:17 +0530

    Write a program to add two complex numbers by reading the numbers from the user

---
+++ 07.py
@@ -0,0 +1,10 @@
+x = int(input("Enter the real part of first complex number: "))
+y = int(input("Enter the imaginary part of first complex number: "))
+
+print(x,"+",y,"i")
+
+a = int(input("Enter the real part of second complex number: "))
+b = int(input("Enter the imaginary part of second complex number: "))
+
+print(a,"+",b,"i")
+print("Complex number after addition:",(x+a),"+",(y+b),"i")
