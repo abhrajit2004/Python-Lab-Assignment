1703a461b94d7f7642ace5f2879777af9bfed485
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-04 03:40:40 +0530

    Write a program to merge two dictionaries

---
+++ 44.py
@@ -0,0 +1,6 @@
+dictionary1 = {"Abhra":500000,"Arpan":800000}
+dictionary2 = {"Suka":700000,"Shoob":600000}
+
+merged_dictionary = {**dictionary1, **dictionary2}
+
+print("The merged dictionary is:",merged_dictionary)
