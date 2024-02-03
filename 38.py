dbf453b614f4b944e679ebef98400927070becfe
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-04 03:02:04 +0530

    Write a program to find the distinct pair of numbers whose product is even from a tuple of integers

---
+++ 38.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = [x for x in numbers if x%2 == 0]
+result = []
+
+for i in range(len(even_numbers)):
+    for j in range(i+1, len(even_numbers)):
+        result.append((even_numbers[i],even_numbers[j]))
+
+print("The distinct pairs from the list", numbers,"are:", result)
