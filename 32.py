4ca91fe0825c4f97f2456c0db1104ff7040f0ccf
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-02-04 02:42:08 +0530

    Write a program to find the distinct pair of numbers whose product is odd from a list of integers

---
+++ 32.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5]
+odd_numbers = [x for x in numbers if x%2 == 1]
+result = []
+
+for i in range(len(odd_numbers)):
+    for j in range(i+1, len(odd_numbers)):
+        result.append((odd_numbers[i], odd_numbers[j]))
+
+print("The distinct pairs from the list", numbers,"are:", result)
